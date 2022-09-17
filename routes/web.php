842a1273f18f3fc46b16032af2c1a2d3dbeab14b
Leandro Santos <lds.leosantos@gmail.com>
2022-09-17 12:15:33 -0300

    02 - Relacionamentos de Tabelas  - 05 - Laravel Relacionamento One to One

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\{
+    User,
+    Preference
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/one-to-one', function () {
+    $user =  User::with('preference')->find(3);
+    $data = [
+        'background_color' => '#fff',
+    ];
+
+    if ($user->preference) {
+
+        $user->preference->update($data);
+    } else {
+        //$user->preference()->create([$data]);
+
+        $preference = new Preference($data);
+        $user->preference()->save($preference);
+
+    }
+    //$preference = $user->preference;
+
+
+    $user->refresh();
+
+    $user->preference->delete();
+    $user->refresh();
+
+    dd($user->preference);
 });
